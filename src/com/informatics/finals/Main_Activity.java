da797ea49c1e4317f52a1c910cd6515cd27c4c96
arkangelx <akisanya6@hotmail.com>
2011-01-06 10:50:26 +0000

    1049 @ work ...test

---
+++ src/com/informatics/finals/Main_Activity.java
@@ -0,0 +1,13 @@
+package com.informatics.finals;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main_Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}/***********/
