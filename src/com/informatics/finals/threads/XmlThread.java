da8596f22831f3b2c7f025556ea82ed5e388593b
arkangelx <akisanya6@hotmail.com>
2011-01-07 18:33:01 +0000

    Began Thread Class

---
+++ src/com/informatics/finals/threads/XmlThread.java
@@ -0,0 +1,22 @@
+package com.informatics.finals.threads;
+
+
+
+public class XmlThread extends Thread{
+	private int PENDING=0;
+	private int STARTED=1;
+	private int FINISHED=2;
+	
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+
+	
+	private int getStatus(){
+	return 0;
+	}
+}
