6e09cae27c5b2cf95944c10c358642a329d45b9a
arkangelx <akisanya6@hotmail.com>
2011-01-07 15:13:20 +0000

    Just made a small addition to the window so that that we get the full screen.

--- src/com/informatics/finals/Main_Activity.java
+++ src/com/informatics/finals/Main_Activity.java
@@ -3,12 +3,18 @@ package com.informatics.finals;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Main_Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//interesting point here
         setContentView(R.layout.main);
+        
     }
 }
