89c178fbf8667346449bd9bb4ea01e50dce3f902
arkangelx <akisanya6@hotmail.com>
2011-01-12 01:40:22 +0000

    Nothing much here

--- src/com/informatics/finals/Main_Activity.java
+++ src/com/informatics/finals/Main_Activity.java
@@ -3,14 +3,17 @@ package com.informatics.finals;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class Main_Activity extends Activity {
+	
+	private static String TAG = "Main_Activity";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	
+    	Log.d(TAG, "oncreate invoked" + savedInstanceState);
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//interesting point here
--- src/com/informatics/finals/threads/XmlThread.java
+++ src/com/informatics/finals/threads/XmlThread.java
@@ -1,17 +1,21 @@
 package com.informatics.finals.threads;
 
+import android.util.Log;
+
 
 
 public class XmlThread extends Thread{
 	private int PENDING=0;
 	private int STARTED=1;
 	private int FINISHED=2;
+	private static String TAG = "XmlThread";
 	
 	
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		Log.d(TAG,"Run in thread invoked"+this.toString());
 		super.run();
 	}
 
